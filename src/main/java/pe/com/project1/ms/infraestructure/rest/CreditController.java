3c3c596be50564deb42e1a1d4c528f489533598e
Jesus Alarcon <jesus.leo0508@gmail.com>
2022-04-04 11:27:01 -0500

    add: docker

--- src/main/java/pe/com/project1/ms/infraestructure/rest/CreditController.java
+++ src/main/java/pe/com/project1/ms/infraestructure/rest/CreditController.java
@@ -1,10 +1,12 @@
 package pe.com.project1.ms.infraestructure.rest;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -22,11 +24,13 @@ public class CreditController {
 	private final CreditService creditService;
 	
 	@GetMapping("customer-id/{customerId}")
+	@ResponseStatus(HttpStatus.OK)
 	public Flux<Credit> getAllByCustomerId(@PathVariable String customerId) {
 		return creditService.findAllByCustomerId(customerId);
 	}
 	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public Mono<Credit> postCredit(@RequestBody CreditRequest creditRequest) {
 		return creditService.save(creditRequest.toCredit());
 	}
